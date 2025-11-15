9563c46cd5bb596f997742455c234807fe9c3f19
Egesimba Miracle <124133399+miracleszn@users.noreply.github.com>
2025-11-15 02:32:14 +0100

    removed the import statement of puzzle design from app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import {React} from 'react'
 import './App.css'
 import Navbar from './components/navbar'
-import PuzzleDesign from './components/puzzle_design'
+
 import Hero from './components/Hero'
 import About from './components/About'
 import Showcase from './components/showcase'
-import Test from './components/Test'
+
 import Footer from './components/Footer'
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
