1b62d674c822faf902791ab59819b8d6c3621e32
miracleszn <miraclediamond99@gmail.com>
2026-03-31 10:30:58 +0100

    in safari browser, the SocialBar.jsx is not centering

--- src/components/SocialBar.jsx
+++ src/components/SocialBar.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function SocialBar({ gapValue }) {
   return (
-    <div className={`flex gap-${gapValue} bg-[#ccb877] h-11 w-max px-6 py-1 rounded-2xl`}>
+    <div className={`flex justify-center gap-${gapValue} bg-[#ccb877] h-11 w-max px-6 py-1 rounded-2xl`}>
         <img onClick={() => window.open("https://www.linkedin.com/in/miracleszn/", "_blank")} className='w-10' src="/icons/icons8-linkedin.svg" alt="" />
         <img onClick={() => window.open("https://x.com/miraclecodes", "_blank")} className='w-10' src="/icons/icons8-x.svg" alt="" />
         <img onClick={() => window.open("https://github.com/miracleszn", "_blank")} className='w-10' src="/icons/icons8-github.svg" alt="" />
